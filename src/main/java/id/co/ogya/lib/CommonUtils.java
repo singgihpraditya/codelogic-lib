eae12e35b8c548e3572ec084dc4c6d495ce60ef6
singgih praditya <singgihpraditya@gmail.com>
2024-09-11 13:38:16 +0700

    remove some method

--- src/main/java/id/co/ogya/lib/CommonUtils.java
+++ src/main/java/id/co/ogya/lib/CommonUtils.java
@@ -3,10 +3,10 @@ package id.co.ogya.lib;
 import java.util.UUID;
 
 public class CommonUtils {
-	public String getLongGeneratedId() {
-		String generatedId = UUID.randomUUID().toString().replace("-", "");
-		return generatedId;
-	}
+//	public String getLongGeneratedId() {
+//		String generatedId = UUID.randomUUID().toString().replace("-", "");
+//		return generatedId;
+//	}
 	
 	public String getShortGeneratedId() {
 		String generatedId = getLongGeneratedId();
